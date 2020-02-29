d3b634f15daa1af8ebf4f52e5d9258eff7a793c1
Stijn Van Bever <svanbever1@gmail.com>
2020-02-29 12:57:20 +0100

    First commit after Spring Initializr

---
+++ src/main/java/be/stijnvanbever/messages/MessagesApiApplication.java
@@ -0,0 +1,13 @@
+package be.stijnvanbever.messages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/stijnvanbever/messages/MessagesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.stijnvanbever.messages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
