27c8819fc42310f60941190baeeb928b8fc87e57
Stijn Van Bever <svanbever1@gmail.com>
2020-03-07 20:07:09 +0100

    Remove unused methods from repository

--- src/integration-test/java/be/stijnvanbever/messages/persistence/MessageRepositoryIT.java
+++ src/integration-test/java/be/stijnvanbever/messages/persistence/MessageRepositoryIT.java
@@ -1,10 +1,12 @@
 package be.stijnvanbever.messages.persistence;
 
+import be.stijnvanbever.messages.model.Message;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,12 +23,14 @@ public class MessageRepositoryIT {
     @ParameterizedTest
     @CsvSource({"Farnsworth,1", "Bender,1", "Hermes,1", "Fry,2"})
     public void shouldReturnMessages_When_SearchingOnSender(String sender, Integer expectedSize) {
-        assertThat(messageRepository.findBySender(sender)).hasSize(expectedSize);
+        Message message = new Message(sender, null, null, null, null);
+        assertThat(messageRepository.findAll(Example.of(message))).hasSize(expectedSize);
     }
 
     @ParameterizedTest
     @CsvSource({"Hermes,0","Farnsworth,1", "Bender,2", "Fry,2"})
     public void shouldReturnMessages_When_SearchingOnReceiver(String receiver, Integer expectedSize) {
-        assertThat(messageRepository.findByReceiver(receiver)).hasSize(expectedSize);
+        Message message = new Message(null, receiver, null, null, null);
+        assertThat(messageRepository.findAll(Example.of(message))).hasSize(expectedSize);
     }
 }
--- src/main/java/be/stijnvanbever/messages/persistence/MessageRepository.java
+++ src/main/java/be/stijnvanbever/messages/persistence/MessageRepository.java
@@ -3,9 +3,5 @@ package be.stijnvanbever.messages.persistence;
 import be.stijnvanbever.messages.model.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface MessageRepository extends MongoRepository<Message, String> {
-    List<Message> findByReceiver(String receiver);
-    List<Message> findBySender(String sender);
 }
